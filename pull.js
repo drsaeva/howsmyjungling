2fbde70f077e57f24cfd26ad0ced72692e01ff11
David R. Saeva <drsaeva@gmail.com>
2016-04-06 17:29:06 -0400

    Update pull.js

--- pull.js
+++ pull.js
@@ -1,11 +1,9 @@
-$.getJSON( "ajax/test.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+var matchID = prompt("Enter a match ID to call via the match API"); 
+/* var matchID =  2134410987 */
+var api_key = prompt("Enter your api key provided by Riot");
+/* var api_key = 7a70a3f5-72e7-4286-807d-a3aca0e2c863 */
+var matchAPI = 'https://na.api.pvp.net/api/lol/na/v2.2/match/' + matchID +'?api_key=' +api_key;
+
+$.getJSON( matchAPI, null, function( results ) {
+  
 });
